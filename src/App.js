d4fe4caa84056b4adfb6918ac8f560c7980ac82a
Vadym <Kylaev1805@gmail.com>
2024-01-07 23:17:45 +0200

    modal_window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import ModalWindow from './components/ModalWindow/ModalWindow';
+import "./App.css";
 
 function App() {
+
+  const[modalState, setModalState] = useState(false);
+  const[currentId, setCurrentId] = useState('');
+  const[data, setData] = useState([
+   {
+    name: 'Andriy',
+    id: '0'
+   },
+   {
+    name: 'Oleg',
+    id: '1'
+   },
+   {
+    name: 'Anton',
+    id: '2'
+   },
+   {
+    name: 'Vadym',
+    id: '3'
+   }
+  
+  ])
+
+  const removeUser = (id) => {
+    setCurrentId(id);
+    setModalState(true);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ModalWindow 
+      call = {modalState} 
+      currentUserId={currentId} 
+      onDestroy={() => setModalState(false)}
+      users={data}
+      onDeleteUserFromData={setData}
+      />
+      <button className='open_modal' onClick={() => setModalState(true)}>Open Modal</button>
+
+      <div className='users'>
+      <ul>
+        {data.map(obj => {
+          return <li key={obj.id}>
+            {obj.name}
+            <button className='remove' onClick={() => removeUser(obj.id)}>X</button>
+            </li>
+        })}
+      </ul>
+      </div>
+
+      
     </div>
   );
 }
